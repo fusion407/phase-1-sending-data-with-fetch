47b785b14dc9a40a2c33afcb8e3592d3d086dff0
fusion407 <fusion407@gmail.com>
2022-08-01 00:27:59 -0500

    commit tree 54dbe1c4fe1a06ec347773d3463be3f925ea8921

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+const userData = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+const element = document.querySelector('body');
+
+function submitData(name, email) {
+    const XHR = new XMLHttpRequest();
+    return fetch("http://localhost:3000/users", {
+        method : 'POST',
+        headers : {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(res => res.json())
+    .then(data => element.append(data.id))
+    .catch(error => {
+        element.append(error.message)
+    })
+}
